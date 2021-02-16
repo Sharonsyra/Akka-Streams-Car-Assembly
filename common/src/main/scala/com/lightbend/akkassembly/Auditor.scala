29a50886e23dc8009093275d3bc2e5c423085edc
sharonsyra <sharonwaithii@gmail.com>
2021-02-16 12:24:00 +0300

    Sink exercise

---
+++ common/src/main/scala/com/lightbend/akkassembly/Auditor.scala
@@ -0,0 +1,17 @@
+package com.lightbend.akkassembly
+
+import akka.Done
+import akka.event.LoggingAdapter
+import akka.stream.scaladsl.Sink
+
+import scala.concurrent.Future
+
+class Auditor {
+  val count: Sink[Any, Future[Int]] = Sink.fold[Int, Any](0) {
+    case(count, _) => count + 1
+  }
+
+  def log(implicit loggingAdapter: LoggingAdapter): Sink[Any, Future[Done]] = Sink.foreach[Any]{
+    i => loggingAdapter.debug(i.toString)
+  }
+}
